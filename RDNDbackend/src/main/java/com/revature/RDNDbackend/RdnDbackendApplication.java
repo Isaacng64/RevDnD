f9d598f259c43512ba92c7b66ae57037773c294b
Santiago Hincapie <santiagohr81@hotmail.com>
2022-01-21 12:36:06 -0500

    basic backend

---
+++ RDNDbackend/src/main/java/com/revature/RDNDbackend/RdnDbackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature.RDNDbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdnDbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdnDbackendApplication.class, args);
+	}
+
+}
---
+++ RDNDbackend/src/test/java/com/revature/RDNDbackend/RdnDbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.RDNDbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdnDbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
