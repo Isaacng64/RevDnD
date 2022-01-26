4a147671c57993468d943da973a0f72983ca02ee
HellPwnage1337 <hellpwnage1337@gmail.com>
2022-01-26 11:33:43 -0600

    user update v1 (backend only)

--- RDNDbackend/src/main/java/com/revature/RDNDbackend/controller/userController.java
+++ RDNDbackend/src/main/java/com/revature/RDNDbackend/controller/userController.java
@@ -4,6 +4,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,7 @@ public class userController {
 	 public void register(@RequestParam("username") String username, @RequestParam("password") String password, 
 			 @RequestParam("email") String email, @RequestParam("phone") String phone)
 	 {
+		System.out.println("Client connected!");
 		
 		 SessionFactory sf= new Configuration().configure().buildSessionFactory();
 		Session session = sf.openSession();
@@ -28,11 +31,24 @@ public class userController {
 		 u.setPassword(password);
 		 u.setEmail(email);
 		 u.setPhone(phone);
-		
+		 System.out.println(u);
 		 session.save(u);
 		 session.getTransaction().commit();
+		 session.close();
 		
-		System.out.println("Client connected!");
-		System.out.println(u);
 	 }
+	
+	@PostMapping("/userupdate")
+	public void update(@RequestBody User user)
+	{
+		System.out.println("Client connected!");
+		SessionFactory sf= new Configuration().configure().buildSessionFactory();
+		Session session = sf.openSession();
+		 session.beginTransaction();
+		 session.update(user);
+		 System.out.println(user);
+		 session.getTransaction().commit();
+		 session.close();
+		 
+	}
 }
