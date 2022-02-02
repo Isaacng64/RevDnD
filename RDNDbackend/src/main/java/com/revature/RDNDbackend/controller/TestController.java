20e5245aefb92ee320eab261f05956a9d7ea92e0
go-emanuel <emanuelbotello2@gmail.com>
2022-02-02 10:07:48 -0800

    new developing branch

--- RDNDbackend/src/main/java/com/revature/RDNDbackend/controller/TestController.java
+++ RDNDbackend/src/main/java/com/revature/RDNDbackend/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.revature.RDNDbackend.controllers;
+package com.revature.RDNDbackend.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
