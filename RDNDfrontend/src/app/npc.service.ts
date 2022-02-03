526cdc8dc093b738cf6f337d1c7d0fdab0403fcd
HellPwnage1337 <hellpwnage1337@gmail.com>
2022-02-03 14:17:39 -0600

    c

---
+++ RDNDfrontend/src/app/npc.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Npc} from './npc';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NpcService {
+
+  private baseURL = "http://localhost:8086/rdnd/npcs";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getNpcList(): Observable<Npc[]>{
+    return this.httpClient.get<Npc[]>(`${this.baseURL}`);
+  }
+
+  createNpc(npc: Npc): Observable<Object>{
+    return this.httpClient.post(`${this.baseURL}`, npc);
+  }
+
+  getNpcById(npcId: number): Observable<Npc>{
+    return this.httpClient.get<Npc>(`${this.baseURL}/${npcId}`);
+  }
+
+  updateNpc(npcId: number, npc: Npc): Observable<Object>{
+    return this.httpClient.put(`${this.baseURL}/${npcId}`, npc);
+  }
+
+  deleteNpc(npcId: number): Observable<Object>{
+    return this.httpClient.delete(`${this.baseURL}/${npcId}`);
+  }
+}
