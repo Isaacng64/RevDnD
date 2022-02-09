ad0641b98d8d442b66820283359b7e82562ae3ec
rafael <rlousta13@gmail.com>
2022-02-09 16:06:55 -0600

    Asdf

--- RDNDfrontend/src/app/components/add-characther/add-characther.component.ts
+++ RDNDfrontend/src/app/components/add-characther/add-characther.component.ts
@@ -2,13 +2,14 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { CharacterService } from 'src/app/services/character.service';
 import { Character } from 'src/app/models/character.model';
+import { CharacterService } from 'src/app/services/character.service';
 
 @Component({
   selector: 'app-add-characther',
   templateUrl: './add-characther.component.html',
-  styleUrls: ['./add-characther.component.css']
+  styleUrls: ['./add-characther.component.css'],
+  providers: [CharacterService]
 })
 
 export class AddCharactherComponent implements OnInit {
@@ -17,20 +18,29 @@ export class AddCharactherComponent implements OnInit {
   charactherService: any;
 
   constructor(
-    private router: Router) { }
+    private router: Router, private characterService: CharacterService) { }
 
   ngOnInit(): void {
   }
 
   saveCharacther() {
+    this.characterService.save(this.characther).subscribe((data: any) => {
 
-    this.charactherService.save(this.characther).subscribe((data: any) => {
       console.log(data);
-    })
-    this.goToCharactherList();
+      this.goToCharactherList();
 
+    })
   }
 
+  /*
+    saveNpc() {
+      this.npcService.createNpc(this.npc).subscribe(data => {
+        console.log(data);
+        this.goToNpcList();
+      },
+        error => console.log(error));
+    }
+  */
   goToCharactherList() {
     this.router.navigate(['/characthers']);
   }
